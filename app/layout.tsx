4dc58cef5056be5a6a7a6d7b2abef391f756b661
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2025-08-26 19:03:37 +0100

    font import problems

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,3 @@
-// import '@/app/ui/global.css';
-// import { inter } from '@/app/ui/fonts';
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-      
-//       <body className={`${inter.className} antialiased`}>{children}</body>
-//     </html>
-//   );
-// }
-
 
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';

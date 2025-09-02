c15dd4d15f92af8e470718bdf12416f1bd991a6d
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2025-09-02 16:23:05 +0100

    links figuring out  usepathname seems like  extra work file name changed from nav-links to my-nav-links

--- app/ui/dashboard/my-nav-links.tsx
+++ app/ui/dashboard/my-nav-links.tsx
@@ -1,8 +1,15 @@
+
+
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+"use client"
+
+import { usePathname } from 'next/navigation';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -14,7 +21,9 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+
 ];
+const pathname = usePathname()
 
 export default function NavLinks() {
   return (
@@ -22,14 +31,14 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
